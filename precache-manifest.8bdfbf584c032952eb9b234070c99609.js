c340fd547d41dcd04a9d2e2740683fc906c5be9c
Fawwaz <fawazpro27@gmail.com>
2019-11-17 16:12:58 +0000

    vuex used

---
+++ precache-manifest.8bdfbf584c032952eb9b234070c99609.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e34b56e7e3f30027d51f",
+    "url": "/css/chunk-vendors.869ee424.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "57293130f1c3a7b87c1a98104646c305",
+    "url": "/img/vuetify.57293130.svg"
+  },
+  {
+    "revision": "b6d6f45cd1f7c23d479322649a528721",
+    "url": "/index.html"
+  },
+  {
+    "revision": "069a7bd95e11947d414a",
+    "url": "/js/about.d5581230.js"
+  },
+  {
+    "revision": "3d074bc6c1079c656298",
+    "url": "/js/app.a0ab9534.js"
+  },
+  {
+    "revision": "e34b56e7e3f30027d51f",
+    "url": "/js/chunk-vendors.cdcc88da.js"
+  },
+  {
+    "revision": "78e9f56c29ebfee7cdbeb0a5cd8a3129",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
